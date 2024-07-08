b764fd316993b7dd3c7260859712585799ab00ed
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-07-08 09:29:54 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// import the CLI class from the lib/cli module
 const CLI = require("./lib/cli");
 
+// create a new instance of the CLI class
 const myCLI = new CLI()
+// Run the CLI application by calling the run method
 myCLI.run();
